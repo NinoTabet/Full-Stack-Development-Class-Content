e93f46346846d29ffcbdf4289ee8cfda0dc664fe
Nino <anthonydavidtabet@gmail.com>
2024-07-29 15:16:37 +0700

    Monday week 4 - changed assignment

--- JavascriptStudy/Dom manipulation/assignment.js
+++ JavascriptStudy/Dom manipulation/assignment.js
@@ -1,14 +1,18 @@
 /*
 
+    Using JS only (unless otherwise specified);
+
     create an div with background red.
     within that div, create an image and add it's src using javascript
-    create a css file that has the following class:
 
-    .center{
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,-50%);
-    }
+    <not using js>
+        create a css file that has the following class:
+
+        .center{
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+        }
 
     add this class to the div and img tags.
 
@@ -16,7 +20,14 @@
     this h1 tag should say "hi everyone".
 
     add a p tag that has a span inside. 
-
     span tag should have a strong tag inside it and say "this is a very strong text".
+    give the p tag a background color of red and text white.
+
+    give an id to the div class that says 'div-id'.
+
+    create a div with 3 span tag children all with their own unique ids.
+    remove the 2nd span tag using only it's id.
+    the other 2 span tags should have "span tag" written inside them.
+    the text for these 2 span tags should be red.
 
 */
