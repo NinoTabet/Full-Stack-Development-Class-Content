bef53708ed47c10025a3b4c06c0acbb21ed3137b
Nino <anthonydavidtabet@gmail.com>
2024-07-30 14:55:28 +0700

    Tuesday week 4 assignment

---
+++ JavascriptStudy/event handling/assignment.js
@@ -0,0 +1,22 @@
+/*
+
+Main task:
+
+    Add at least 4 mouse listeners to your travel website.
+    Add at lest 1 keyboard listener to your travel website.
+
+    Example mouse listener: 
+
+    1) Button changes colour on mouse down
+    2) On mouse over change the border colour of a div, table, or whatever
+    3) On mouse move display it's position in a p tag
+
+
+Bonus task:
+
+    Using event listeners and what we discussed in class yesterday, create a mouse trail
+    that will follow the mouse around the screen.
+    
+    On mouse move display it's position in a p tag on displayed on screen
+
+*/
