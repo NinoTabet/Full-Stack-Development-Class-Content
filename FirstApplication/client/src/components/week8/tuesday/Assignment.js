5860cf7e72faeb638f67590711de391b34f93fa6
Nino <anthonydavidtabet@gmail.com>
2024-08-27 15:14:10 +0700

    Tuesday week 8

---
+++ FirstApplication/client/src/components/week8/tuesday/Assignment.js
@@ -0,0 +1,27 @@
+/*
+Create 3 components (Home, About, Contact)
+    
+Your home page should have the following:
+    H1 tag
+    P tag
+    Button
+    2 links (about and contact)
+        Notes: 
+            all should be centered on the page using bootstrap
+            when the button is clicked you should change the title to your name (use useState)
+
+
+Your About page should have the following:
+    h1 tag
+    p tag
+        Notes: 
+            All should be centered on the page using bootstrap
+
+Your Contact page should have the following:
+    Form (name, email, textarea, submit button)
+    When submit button is clicked, send a POST request to /contactFormSubmission
+    The Body of the POST request should have the content of the form.
+        Notes:
+            All should be centered on the page using bootstrap
+
+*/
